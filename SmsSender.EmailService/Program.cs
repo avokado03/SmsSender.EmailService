d37bfa09f5ef99f64338d6fa024aa9b5f3b1c8f3
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2022-11-20 14:30:25 +0300

    Добавьте файлы проекта.

---
+++ SmsSender.EmailService/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SmsSender.Common.RabbitMQ;
+using System.Reflection;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        var environment = context.HostingEnvironment;
+
+        config.SetBasePath(environment.ContentRootPath)
+              .AddJsonFile("appsettings.json", false, true)
+              .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true)
+              .AddEnvironmentVariables()
+              .AddUserSecrets(Assembly.GetExecutingAssembly());
+    });
+builder.ConfigureServices((context, services) =>
+    services.ConfigureRabbit(context.Configuration))
+.Build();
+
